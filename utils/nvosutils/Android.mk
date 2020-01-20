79b52acd137ee8328a85ecc3f8d1b6dbb322b306
C457 <android.c357@gmail.com>
2020-01-20 12:50:16 -0600

    nvosutils: Include NV common core-headers

--- utils/nvosutils/Android.mk
+++ utils/nvosutils/Android.mk
@@ -2,16 +2,24 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+COMMON_C_INCLUDES := hardware/nvidia/core-headers
+
+include $(CLEAR_VARS)
+
 LOCAL_MODULE := libnvosutils
-LOCAL_SRC_FILES += nvustring.c
-LOCAL_SRC_FILES += nvuhash.c
+
+LOCAL_C_INCLUDES += $(COMMON_C_INCLUDES)
+
+LOCAL_SRC_FILES := nvustring.c nvuhash.c
 
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libnvosutils
-LOCAL_SRC_FILES += nvustring.c
-LOCAL_SRC_FILES += nvuhash.c
+
+LOCAL_C_INCLUDES += $(COMMON_C_INCLUDES)
+
+LOCAL_SRC_FILES := nvustring.c nvuhash.c
 
 include $(BUILD_HOST_STATIC_LIBRARY)
