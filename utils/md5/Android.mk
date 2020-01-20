1fd8553509abbce15d4ae80d6739b4da6afc312c
C457 <android.c357@gmail.com>
2020-01-20 12:58:27 -0600

    md5: Include NV common core-headers

--- utils/md5/Android.mk
+++ utils/md5/Android.mk
@@ -2,7 +2,13 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+COMMON_C_INCLUDES := hardware/nvidia/core-headers
+
+include $(CLEAR_VARS)
+
 LOCAL_MODULE := libmd5
+
+LOCAL_C_INCLUDES := $(COMMON_C_INCLUDES)
 
 LOCAL_SRC_FILES += md5.c
 
@@ -12,5 +18,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libmd5_host_static
 
+LOCAL_C_INCLUDES := $(COMMON_C_INCLUDES)
+
 LOCAL_SRC_FILES += md5.c
+
 include $(BUILD_HOST_STATIC_LIBRARY)
